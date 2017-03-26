8bb6213cad33bf5b3a71791fd0977bc5c4921fd9
S-H-GAMELINKS <gamelinks007@gmail.com>
2017-03-26 23:55:41 +0900

    バックログ関連の処理を関数にて細分化

--- GAME.cpp
+++ GAME.cpp
@@ -2304,6 +2304,115 @@ void AUTO_SKIP_STOP() {
 	}
 }
 
+//バックログ参照メッセージ
+void BACKLOG_MESSAGE() {
+
+	SAVE = MessageBox(
+		NULL,
+		"バックログ画面に移行しますか？",
+		"ゲームリンクス制作のノベルゲームエンジン「LINKS」",
+		MB_YESNO
+	);
+}
+
+//バックログ(キー操作関連)
+void BACKLOG_KEY_MOVE() {
+
+	//バックログ（キー操作関連）
+	if (LOG != 10 && CheckHitKey(KEY_INPUT_UP) == 1 || LOG != 10 && (GetMouseInput() & MOUSE_INPUT_LEFT) != 0) {
+		LOG++;
+		WaitTimer(300);
+	}
+
+	if (LOG != 1 && CheckHitKey(KEY_INPUT_DOWN) == 1 || LOG != 1 && (GetMouseInput() & MOUSE_INPUT_RIGHT) != 0) {
+		LOG--;
+		WaitTimer(300);
+	}
+}
+
+//バックログの描画関数
+void BACKLOG_SCREENSHOT_DRAW() {
+
+	if (LOG == 1) {
+		DrawGraph(0, 0, BACKLOG[0], TRUE);
+		DrawString(0, 450, "バックログ1", Cr);
+	}
+	if (LOG == 2) {
+		DrawGraph(0, 0, BACKLOG[1], TRUE);
+		DrawString(0, 450, "バックログ2", Cr);
+	}
+	if (LOG == 3) {
+		DrawGraph(0, 0, BACKLOG[2], TRUE);
+		DrawString(0, 450, "バックログ3", Cr);
+	}
+	if (LOG == 4) {
+		DrawGraph(0, 0, BACKLOG[3], TRUE);
+		DrawString(0, 450, "バックログ4", Cr);
+	}
+	if (LOG == 5) {
+		DrawGraph(0, 0, BACKLOG[4], TRUE);
+		DrawString(0, 450, "バックログ5", Cr);
+	}
+	if (LOG == 6) {
+		DrawGraph(0, 0, BACKLOG[5], TRUE);
+		DrawString(0, 450, "バックログ6", Cr);
+	}
+	if (LOG == 7) {
+		DrawGraph(0, 0, BACKLOG[6], TRUE);
+		DrawString(0, 450, "バックログ7", Cr);
+	}
+	if (LOG == 8) {
+		DrawGraph(0, 0, BACKLOG[7], TRUE);
+		DrawString(0, 450, "バックログ8", Cr);
+	}
+	if (LOG == 9) {
+		DrawGraph(0, 0, BACKLOG[8], TRUE);
+		DrawString(0, 450, "バックログ9", Cr);
+	}
+	if (LOG == 10) {
+		DrawGraph(0, 0, BACKLOG[9], TRUE);
+		DrawString(0, 450, "バックログ10", Cr);
+	}
+}
+
+//バックログ参照
+void BACKLOG_DRAW() {
+
+	//バックログ参照メッセージ
+	BACKLOG_MESSAGE();
+
+	if (SAVE == IDYES) {
+
+		WaitTimer(600);
+
+		LOG = 1;
+
+		while (ProcessMessage() == 0) {
+
+			//バックログ（キー操作関連）
+			BACKLOG_KEY_MOVE();
+
+			//バックログの描画関数
+			BACKLOG_SCREENSHOT_DRAW();
+
+			//画面クリア処理
+			SCREEN_CLEAR();
+
+			//終了処理
+			if (CheckHitKey(KEY_INPUT_RETURN) == 1 || (GetMouseInput() & MOUSE_INPUT_RIGHT) != 0 && (GetMouseInput() & MOUSE_INPUT_LEFT) != 0) {
+
+				ClearDrawScreen();
+
+				DeleteGraph(BACKLOG[0]);
+
+				WaitTimer(300);
+
+				break;
+			}
+		}
+	}
+}
+
 //ゲームメニュー
 int GAMEMENU() {
 
@@ -2423,90 +2532,8 @@ int GAMEMENU() {
 			//バックログ参照
 			if (GAME_y == (GAMEMENU_y * 8) && CheckHitKey(KEY_INPUT_RETURN) == 1 || GAME_y == (GAMEMENU_y * 8) && ((GetMouseInput() & MOUSE_INPUT_LEFT) != 0)) {
 
-				SAVE = MessageBox(
-					NULL,
-					"バックログ画面に移行しますか？",
-					"ゲームリンクス制作のノベルゲームエンジン「LINKS」",
-					MB_YESNO
-					);
-
-				if (SAVE == IDYES) {
-
-					WaitTimer(600);
-
-					LOG = 1;
-
-					while (ProcessMessage() == 0) {
-
-						if (LOG != 10 && CheckHitKey(KEY_INPUT_UP) == 1 || LOG != 10 && (GetMouseInput() & MOUSE_INPUT_LEFT) != 0) {
-							LOG++;
-							WaitTimer(300);
-						}
-
-						if (LOG != 1 && CheckHitKey(KEY_INPUT_DOWN) == 1 || LOG != 1 && (GetMouseInput() & MOUSE_INPUT_RIGHT) != 0) {
-							LOG--;
-							WaitTimer(300);
-						}
-
-						if (LOG == 1) {
-							DrawGraph(0, 0, BACKLOG[0], TRUE);
-							DrawString(0, 450, "バックログ1", Cr);
-						}
-						if (LOG == 2) {
-							DrawGraph(0, 0, BACKLOG[1], TRUE);
-							DrawString(0, 450, "バックログ2", Cr);
-						}
-						if (LOG == 3) {
-							DrawGraph(0, 0, BACKLOG[2], TRUE);
-							DrawString(0, 450, "バックログ3", Cr);
-						}
-						if (LOG == 4) {
-							DrawGraph(0, 0, BACKLOG[3], TRUE);
-							DrawString(0, 450, "バックログ4", Cr);
-						}
-						if (LOG == 5) {
-							DrawGraph(0, 0, BACKLOG[4], TRUE);
-							DrawString(0, 450, "バックログ5", Cr);
-						}
-						if (LOG == 6) {
-							DrawGraph(0, 0, BACKLOG[5], TRUE);
-							DrawString(0, 450, "バックログ6", Cr);
-						}
-						if (LOG == 7) {
-							DrawGraph(0, 0, BACKLOG[6], TRUE);
-							DrawString(0, 450, "バックログ7", Cr);
-						}
-						if (LOG == 8) {
-							DrawGraph(0, 0, BACKLOG[7], TRUE);
-							DrawString(0, 450, "バックログ8", Cr);
-						}
-						if (LOG == 9) {
-							DrawGraph(0, 0, BACKLOG[8], TRUE);
-							DrawString(0, 450, "バックログ9", Cr);
-						}
-						if (LOG == 10) {
-							DrawGraph(0, 0, BACKLOG[9], TRUE);
-							DrawString(0, 450, "バックログ10", Cr);
-						}
-
-						SetDrawScreen(DX_SCREEN_BACK);
-
-						ClearDrawScreen();
-
-						SetDrawScreen(DX_SCREEN_FRONT);
-
-						if (CheckHitKey(KEY_INPUT_RETURN) == 1 || (GetMouseInput() & MOUSE_INPUT_RIGHT) != 0 && (GetMouseInput() & MOUSE_INPUT_LEFT) != 0) {
-
-							ClearDrawScreen();
-
-							DeleteGraph(BACKLOG[0]);
-
-							WaitTimer(300);
-
-							break;
-						}
-					}
-				}
+				//バックログ参照
+				BACKLOG_DRAW();
 			}
 
 			//設定
