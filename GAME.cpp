144d76208a1724e2ba1db7b0d664b320c619a42e
yumetodo <yume-wikijp@live.jp>
2017-05-29 13:20:28 +0900

    fix: snprintf argument

--- GAME.cpp
+++ GAME.cpp
@@ -240,7 +240,7 @@ static bool SerialNumberFileLoader(int* dest_arr, size_t dest_arr_num, const cha
 #ifdef LINKS_C11_CRT_BOTH_SECURE_FUNCTIONS
 		if(-1 == sprintf_s(FilePathString, dest_arr_num, format, i + 1)) return false;
 #else
-		if(0 > snprintf(FilePathString, dest_arr_num, format, i) return false;
+		if (0 > snprintf(FilePathString, dest_arr_num, format, i + 1) return false;
 #endif
 		dest_arr[i] = LoadFunc(FilePathString);
 	}
@@ -2992,7 +2992,7 @@ void MOVIE_START() {
 #ifdef LINKS_C11_CRT_BOTH_SECURE_FUNCTIONS
 		if (-1 == sprintf_s(MovieFilePath, countof(MovieFilePath), "DATA/MOVIE/MOVIE%c%c.wmv", String[SP][CP], String[SP][CP + 1])) return;
 #else
-		if (0 > snprintf(MovieFilePath, countof(MovieFilePath), format, i) return;
+		if (0 > snprintf(MovieFilePath, countof(MovieFilePath), "DATA/MOVIE/MOVIE%c%c.wmv", String[SP][CP], String[SP][CP + 1]) return;
 #endif
 		PlayMovie(MovieFilePath, 1, DX_MOVIEPLAYTYPE_BCANCEL);
 		CP += 2;
